6c28f3e33c14d2b896a0e17d6cd0b85b478bd434
carlichezz <carlosarielsanchez02@gmail.com>
2026-06-28 17:43:34 -0400

    Dibujar el tablero

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿const int FILAS = 10;
+const int COLUMNAS = 10;
 
-const int FILAS = 20;
-const int COLUMNAS = 20;
-//bool[,] 
+bool[,] tablero = new bool[FILAS,COLUMNAS];
 
-Console.WriteLine(COLUMNAS);
-Console.WriteLine(FILAS);
+tablero[3,4] = true;
+tablero[4,4] = true;
+tablero[4,5]= true;
 
+void dibujar_tablero ()
+{
+    Console.Clear();
+    for (int i = 0;i<FILAS;i++)
+    {
+        for (int j = 0; j<COLUMNAS;j++)
+        {
+            Console.Write(tablero[i,j]  ? "█" : ".");
+
+        }
+        Console.WriteLine();
+    }
+}
+dibujar_tablero();
 Console.ReadKey();
--- obj/Debug/net10.0/GameofLife.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameofLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameofLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57caa7f95faa7dcf60cd3363c9d90d5bc84790e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameofLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameofLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
