51ef855e8f9131a8a3c1faa08e628be5e4293a28
carlichezz <carlosarielsanchez02@gmail.com>
2026-08-04 14:02:06 -0400

    caso que faltaba en la supervivencia de una celula

--- obj/Debug/net10.0/GameofLife.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameofLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameofLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23047edaab6e97009d883bc5bd11368d8861cd8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e40878d4dab7a47fdfd4de4df5a1b731e072d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameofLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameofLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
